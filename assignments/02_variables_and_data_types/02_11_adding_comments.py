f92f12cf6c93b0bbaf9033fb44112b17d932d66d
aaron-cribbin <aaron.cribbin001@albright.edu>
2026-09-07 22:08:12 -0400

    3rd

---
+++ assignments/02_variables_and_data_types/02_11_adding_comments.py
@@ -0,0 +1,19 @@
+"""
+favorite_number = 4
+
+print("For picking this objective, I needed time for picking my favorite number. After thinking about it for a while, I")
+print("concluded that for my favorite number, it is " + str(favorite_number))
+
+"""
+# I tried my best to lead on to my favorite number in the code by using the word "for" cause it sounds like "four"
+# All the code does is state my favorite number, being 4.
+
+"""
+print(2.59+5.41)
+print(12-4)
+print(2*2*2)
+print(4160/520)
+"""
+
+# I found this one interesting because it is just like a calc. (calc is short for calctulator btw)
+# All the code does is preform equations like a calculator. (calc.)
